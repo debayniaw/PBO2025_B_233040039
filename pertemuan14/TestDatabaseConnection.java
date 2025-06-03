7524976cfffc81ad41fee49ef0946e62db939129
debayniaw <146064208+debayniaw@users.noreply.github.com>
2025-06-03 08:43:37 +0700

    Delete pertemuan14 directory

--- pertemuan14/TestDatabaseConnection.java
+++
@@ -1,7 +0,0 @@
-package pertemuan14;
-
-public class TestDatabaseConnection {
-	 public static void main(String[] args) {
-	        System.out.println("Connection String: " + DatabaseConnection.getConnectionString());
-	    }
-}
